34d96631677fa65a2c390562d03788a7bc481ca9
Sara Vieira <hey@iamsaravieira.com>
2018-12-18 23:26:19 +0000

    removed photoshoped

--- src/components/Meme.vue
+++ src/components/Meme.vue
@@ -1,34 +1,46 @@
 <template>
-  <article>
-    <div role="img" aria-label="Primer movie" class="grow aspect-ratio--4x6 " :style="`background: url(${meme.url}) no-repeat center center; background-size: cover;`"></div>
-    <div class="hover pa2">
-      <div class="measure relative">
-        <label for="meme" class=" f6 b mb2">Image URL
-        </label>
-        <input class="input-reset ba b--black-20 pa2 mb2 w-100" type="text" id="meme" :value="meme.url">
-        <button class="f6 link ph3 pv2 mb2 dib white bg-black copy pointer" type="button" v-clipboard:copy="meme.url" v-bind:class="{'bg-green ' : copied }" v-clipboard:success="onCopy">
-          {{copied ? 'Copied!' : 'Copy!'}}
-        </button>
-      </div>
-    </div>
+  <article class="meme">
+    <img :src="meme.url" />
+    <!-- <label
+      for="meme"
+      class=" f6 b mb2"
+    >Image URL
+    </label>
+    <input
+      class="input-reset ba b--black-20 pa2 mb2 w-100"
+      type="text"
+      id="meme"
+      :value="meme.url"
+    >
+    <button
+      class="f6 link ph3 pv2 mb2 dib white bg-black copy pointer"
+      type="button"
+      v-clipboard:copy="meme.url"
+      v-bind:class="{'bg-green ' : copied }"
+      v-clipboard:success="onCopy"
+    >
+      {{copied ? 'Copied!' : 'Copy!'}}
+    </button> -->
   </article>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
       copied: false
-    }
+    };
   },
-  name: 'Meme',
-  props: ['meme'],
+  name: "Meme",
+  props: ["meme"],
   methods: {
-    onCopy: function (e) {
-      this.copied = true
-      window.setTimeout(() => { this.copied = false }, 2000)
+    onCopy: function(e) {
+      this.copied = true;
+      window.setTimeout(() => {
+        this.copied = false;
+      }, 2000);
     }
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 .copy {
@@ -41,6 +53,20 @@ export default {
 }
 
 .meme {
+  overflow: hidden;
+  overflow: hidden;
+  position: relative;
+  height: 100px;
+  cursor: pointer;
+
+  img {
+    width: 200%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+
   &:hover .hover {
     opacity: 1;
   }
--- src/components/Memes.vue
+++ src/components/Memes.vue
@@ -1,24 +1,6 @@
 <template>
   <article>
-    <div class="toggles">
-      <input
-        type="checkbox"
-        name="checkbox"
-        id="checkbox"
-        class="ios-toggle"
-        v-model="photoshopedComputer"
-      />
-      <label
-        for="checkbox"
-        class="checkbox-label"
-        data-off="No Photoshop"
-        data-on="Photoshoped"
-      ></label>
-    </div>
-    <ApolloQuery
-      :query="require('../graphql/getMemes.gql')"
-      :variables="{ photoshoped }"
-    >
+    <ApolloQuery :query="require('../graphql/getMemes.gql')">
       <template slot-scope="{ result: { loading, error, data }, isLoading }">
         <div
           v-if="loading || isLoading"
@@ -28,14 +10,29 @@
           v-else-if="error"
           class="error apollo"
         >An error occured</div>
-        <div v-else-if="data && shuffle(data.memes)">
-          <article
-            v-for="meme in data.memes"
-            :key="meme.id"
-            class="fl w-50 w-33-l link overflow-hidden relative meme sans-serif"
-          >
-            <Meme :meme="meme.photo" />
-          </article>
+        <div
+          v-else-if="data && shuffle(data.memes)"
+          class="container"
+        >
+          <div class="memes">
+            <article
+              v-for="meme in data.memes"
+              :key="meme.id"
+              v-on:click="selected = meme"
+            >
+              <Meme :meme="meme.photo" />
+            </article>
+          </div>
+          <div class="picked">
+            <img
+              v-if="!selected"
+              :src="data.memes[0].photo.url"
+            />
+            <img
+              v-if="selected"
+              :src="selected.photo.url"
+            />
+          </div>
         </div>
         <div
           v-else
@@ -51,7 +48,8 @@ import Meme from "./Meme";
 export default {
   data() {
     return {
-      photoshoped: null
+      photoshoped: null,
+      selected: null
     };
   },
   components: {
@@ -75,7 +73,6 @@ export default {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
       }
-
       return array;
     }
   },
@@ -102,86 +99,20 @@ export default {
   transition: 0.25s ease-in-out;
   outline: none;
 }
-.toggles {
-  width: 60px;
-  margin: 50px auto;
-  text-align: center;
-}
-.ios-toggle,
-.ios-toggle:active {
-  position: absolute;
-  top: -5000px;
-  height: 0;
-  width: 0;
-  opacity: 0;
-  border: none;
-  outline: none;
-}
-.checkbox-label {
-  display: block;
-  position: relative;
-  padding: 10px;
-  margin-bottom: 20px;
-  font-size: 12px;
-  line-height: 16px;
-  width: 100%;
-  height: 36px;
-  border-radius: 18px;
-  background: #f8f8f8;
-  cursor: pointer;
-}
-.checkbox-label:before {
-  content: "";
-  display: block;
-  position: absolute;
-  z-index: 1;
-  line-height: 34px;
-  text-indent: 40px;
-  height: 36px;
-  width: 36px;
-  border-radius: 100%;
-  top: 0px;
-  left: 0px;
-  right: auto;
-  background: white;
-  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2), 0 0 0 2px #dddddd;
-}
-.checkbox-label:after {
-  content: attr(data-off);
-  display: block;
-  position: absolute;
-  z-index: 0;
-  top: 0;
-  left: -300px;
-  padding: 10px;
-  height: 100%;
-  width: 300px;
-  text-align: right;
-  color: #bfbfbf;
-  white-space: nowrap;
-}
-.ios-toggle:checked + .checkbox-label {
-  box-shadow: inset 0 0 0 20px rgba(19, 191, 17, 1),
-    0 0 0 2px rgba(19, 191, 17, 1);
-}
-.ios-toggle:checked + .checkbox-label:before {
-  left: calc(100% - 36px);
-  box-shadow: 0 0 0 2px transparent, 0 3px 3px rgba(0, 0, 0, 0.3);
-}
-.ios-toggle:checked + .checkbox-label:after {
-  content: attr(data-on);
-  left: 60px;
-  width: 36px;
+
+.memes {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
 }
 
-.ios-toggle + .checkbox-label {
-  box-shadow: inset 0 0 0 0px rgba(19, 191, 17, 1), 0 0 0 2px #dddddd;
-}
-.ios-toggle:checked + .checkbox-label {
-  box-shadow: inset 0 0 0 18px rgba(19, 191, 17, 1),
-    0 0 0 2px rgba(19, 191, 17, 1);
-}
-.ios-toggle:checked + .checkbox-label:after {
-  color: rgba(19, 191, 17, 1);
+.container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
+  max-width: 100%;
+  margin: auto;
+  width: 1180px;
+  margin-bottom: 50px;
 }
 </style>
